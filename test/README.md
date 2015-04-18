d9e0c2b5145472e643e276c24e4b169691dae329
Kirk Strauser <kirk@strauser.com>
2015-04-17 18:35:57 -0700

    Initial commit of test cases

---
+++ test/README.md
@@ -0,0 +1,30 @@
+# Creating test cases
+
+Publicly distributable test cases go in the `test/cases` directory. Put private and/or confidential cases in `test/privatecases`, which Git ignores.
+
+Within those directories, place your database files into the `data` subdirectory.
+
+Test cases are JSON files named `[something].json` with defined keys:
+
+* **cmd_args**: the arguments passed to pgdbf to generate the test output. If this is a string, it will be passed as a single argument to pgdbf. If you're attempting to pass more than one argument, *this is probably not what you want*. In that case, set `cmd_args` to a list of string arguments.
+    * With one argument: `"cmd_args": "mytable.dbf"`
+    * With multiple arguments: `"cmd_args": ["-m", "memofile.fpt", "datafile.dbf"]`
+* **head**: a string to be matched against the start of the test output
+* **md5**: the expected MD5 hex digest of the test output
+* **tail**: a string to be matched against the start of the test output
+
+Unknown keys are ignored.
+
+Public test cases are loaded and executed in alphabetical order, followed by private test cases.
+
+# Running test cases
+
+Run the `runtests.py` program *from within the `test/` directory* to execute all unit tests.
+
+If you specify the `-p` argument to choose which pgdbf executable to test, use either an absolute path, or path *relative to being inside the `cases/` or `privatecases/` directory*. `runtests.py` CDs into each directory in turn so that a test case's `cmd_args` value can refer to a relative path like `data/myfile.dbf`, so giving a path relative to `test/` will fail.
+
+For example:
+
+    ./runtests.py ../../src/pgdbf
+
+will test a freshly built-but-not-installed binary.
---
+++ test/runtests.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+
+# pylint: disable=superfluous-parens
+
+"""Run a suite of PgDBF test cases."""
+
+import argparse
+from glob import glob
+from hashlib import md5
+from json import load
+from logging import basicConfig, getLogger, DEBUG, INFO
+from os import chdir, getcwd
+from subprocess import Popen, PIPE, STDOUT
+
+LOGGER = getLogger('')
+
+class TestError(ValueError):
+    """A test failed"""
+
+
+def check_head(expected):
+    """Check that the start of the file is as expected"""
+
+    LOGGER.debug('opened a header check')
+    body = ''
+    length = len(expected)
+    while True:
+        data = yield()
+        if not data:
+            raise ValueError({'error': 'short read', 'expected': length, 'actual': len(body)})
+        body += data
+        if len(body) >= length:
+            actual = body[:length]
+            if expected != actual:
+                raise TestError('unequal head', expected, actual)
+            LOGGER.info('passed the header check')
+            break
+
+    while True:
+        data = yield()
+        if data is None:
+            LOGGER.debug('closed the header check')
+            return
+
+
+def check_length(expected):
+    """Check that the file has the expected length"""
+
+    LOGGER.debug('opened a length check')
+    actual = 0
+    while True:
+        data = yield()
+        if not data:
+            if expected != actual:
+                raise TestError('incorrect length', expected, actual)
+            LOGGER.info('passed the length check')
+            LOGGER.debug('closed the length check')
+            return
+        actual += len(data)
+
+
+def check_md5(expected):
+    """Check that the file has the expected MD5 hash"""
+
+    LOGGER.debug('opened an md5 check')
+    hasher = md5()
+    while True:
+        data = yield()
+        if data is None:
+            actual = hasher.hexdigest()
+            if expected != actual:
+                raise TestError('bad md5 hash', actual, expected)
+            LOGGER.info('passed the md5 check')
+            LOGGER.debug('closed the md5 check')
+            return
+        hasher.update(data)
+
+
+def check_tail(expected):
+    """Check that the end of the file is as expected"""
+
+    LOGGER.debug('opened a tail check')
+    actual = ''
+    length = len(expected)
+    while True:
+        data = yield()
+        if data is None:
+            if expected != actual:
+                raise TestError('incorrect tail', actual, expected)
+            LOGGER.info('passed the tail check')
+            LOGGER.debug('closed the tail check')
+            return
+        actual = (actual + data)[-length:]
+
+
+def run_test(pgdbf_path, config):
+    """Run a test case with the given pgdbf executable"""
+
+    tests = []
+    for key, value in config.items():
+        try:
+            test_func = {
+                'head': check_head,
+                'length': check_length,
+                'md5': check_md5,
+                'tail': check_tail,
+            }[key]
+        except KeyError:
+            pass
+        else:
+            test = test_func(value)
+            next(test)
+            tests.append(test)
+
+    if not tests:
+        raise ValueError('No tests are configured')
+
+    args = config['cmd_args']
+    if not isinstance(args, list):
+        args = [args]
+    command = Popen([pgdbf_path] + args, stdout=PIPE, stderr=STDOUT)
+    while True:
+        chunk = command.stdout.read(128 * 1024)
+        if not chunk:
+            break
+        for test in tests:
+            test.send(chunk)
+
+    for test in tests:
+        try:
+            test.send(None)
+        except StopIteration:
+            pass
+        else:
+            raise ValueError('test {} did not close cleanly'.format(test))
+
+
+def handle_command_line():
+    """Evaluate the command line arguments and run tests"""
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--pgdbf', '-p', help='Path to the pgdbf executable')
+    parser.add_argument('--verbose', '-v', action='count', help='Increase debugging verbosity')
+    args = parser.parse_args()
+    if args.verbose >= 2:
+        basicConfig(level=DEBUG)
+    elif args.verbose == 1:
+        basicConfig(level=INFO)
+    else:
+        basicConfig()
+
+    orig_dir = getcwd()
+    for test_dir in ('cases', 'privatecases'):
+        chdir(test_dir)
+        for case in glob('*.json'):
+            print('Running {}/{}'.format(test_dir, case))
+            with open(case) as infile:
+                run_test(args.pgdbf or 'pgdbf', load(infile))
+        chdir(orig_dir)
+
+
+if __name__ == '__main__':
+    handle_command_line()
